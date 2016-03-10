032fc8830af3c3180cd044e39c0b6afd4a93f66d
Tommy Vo <nghiavo@Nghias-MacBook-Pro-2.local>
2016-03-09 22:15:18 -0500

    Add error handling for delete, also fix the dict

--- BITree.py
+++ BITree.py
@@ -1,41 +1,51 @@
 class BITree:
-    def __init__(self, maxbound = 1000000):
+    def __init__(self, max_bound = 1000000, init_list = []):
         self._ds = {}
-        self._maxbound = maxbound
+        self._max_bound = max_bound
+        self._counter = {}
+        for element in init_list:
+            self.add(element)
 
-    def upTree(self, num, value):
-        if (int(num) > self._maxbound):
+    def _upTree(self, num, value):
+        if (num > self._max_bound):
             return;
         if (num not in self._ds):
-            self._ds[num] = 1
+            self._ds[num] = value
         else:
             self._ds[num] += value
-        num = int(num)
-        self.upTree(str(num + (num & - num)), value)
+        self._upTree(num + (num & - num), value)
 
-    def downTree(self, num):
+    def _downTree(self, num):
         if (num in self._ds):
             tmp = self._ds[num]
         else:
             tmp = 0
-        num = int(num)
         if (num <= 0):
             return tmp
         else:
-            return tmp + self.downTree(str(num - (num & - num)))
+            return tmp + self._downTree(num - (num & - num))
 
     def count(self, x_range, y_range):
         lower_range = min([x_range, y_range])
         upper_range = max([x_range, y_range])
-        lowsum = self.downTree(str(lower_range-1))
-        upsum = self.downTree(str(upper_range))
+        lowsum = self._downTree(lower_range-1)
+        upsum = self._downTree(upper_range)
         return upsum - lowsum
 
     def add(self, num, value = 1):
-        self.upTree(str(num), value)
+        # O(log(max_bound))
+        if (num not in self._counter):
+            self._counter[num] = value
+        else:
+            self._counter[num] += value
+        self._upTree(num, value)
 
     def delete(self, num, value = 1):
-        self.upTree(str(num), value * -1)
+        # O(log(max_bound))
+        if (num not in self._counter) or (self._counter[num] < value):
+            print ("Error: Not enough elements to delete.")
+        self._counter[num] -= value
+        self._upTree(num, value * -1)
 
     def clear(self):
         self._ds.clear()
